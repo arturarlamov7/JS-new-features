1b22491ac6586c199711856af78feb39b3ae91d2
Vladislav <vladislav.koselev@ivkhk.ee>
2021-09-01 13:08:39 +0300

    sshs

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const typeGoods = 'food';
+
+//console.log('https://market.com/' + typeGoods);
+
+//console.log('https://market.com/${typeGoods}');
+
+//console.log(`https://market.com/${typeGoods}`);
+
+
+const games = 'game';
+
+const type = 'type';
+
+//console.log(`https://market.com/${games}`);
+
+console.log(`https://market.com/${games}/${type}`);
