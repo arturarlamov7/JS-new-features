b26a5de34ecf1c4fb64cda6b8a5a869e2eb20017
Artur <58815804+arturarlamov7@users.noreply.github.com>
2021-09-02 08:23:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+//"use strict"
+
+//const typeGoods = 'food';
+
+//console.log('https://market.com/' + typeGoods);
+
+//console.log('https://market.com/${typeGoods}');
+
+//console.log(`https://market.com/${typeGoods}`);
+
+
+//const games = 'game';
+
+//const type = 'type';
+
+//console.log(`https://market.com/${games}`);
+
+//console.log(`https://market.com/${games}/${type}`);
+
+
+//"use strict";
+
+//let number = 10;
+
+//function showMessage(text) {
+    //console.log(text);
+    ////let number = 20;
+    //console.log(number)
+//}
+
+//showMessage("Hello!!!");
+//console.log(number);
+
+//const server = function() {
+    //console.log('server start...');
+//}
+
+//server();
+
+//const calc = (x, y) => x+y;
+//// const calc = (x, y) => {
+////      return x+y    
+////};
+
+////const calc = x => x+10;
+
+//console.log(calc(10,5));
+
+
+// "use strick";
+
+// function server(){
+//     setTimeout(function(){
+//         console.log(1);
+//     },1000)
+// }
+
+// function foo() {
+//             console.log(2);
+//     }
+// server();
+// foo();
+
+// function serverNew(host, callback){
+//     console.log('Server ${host} is starting...');
+//     callback();
+// }
+
+// // serverNew('Myserver', function(){
+// //      console.log('connect success!');
+// // })
+
+// function done() {
+//     console.log('connect success!');
+// }
+
+// serverNew('MyServer', done)
+
+//j
