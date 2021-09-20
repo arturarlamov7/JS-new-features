236740e0eca613ea02253f758a88e193458263f3
Vladislav <vladislav.koselev@ivkhk.ee>
2021-09-20 13:02:31 +0300

    3.4 changed

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 //Fetch API
 
-"use strict";
-
-fetch('https://jsonplaceholder.typicode.com/tools/1')
- .then(response => response.json())
- .then(json => console.log(json));
 
 
- fetch('https://jsonplaceholder.typicode.type.com/posts', {
-     method: "POST",
-     body: JSON.stringify({name: "Alex"}),
-     headers: {
-         'Content-type': 'application/json'
-     }
- })
- .then(response => response.json())
- .then(json => console.log(json));
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json));
+
+
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: "POST",
+      body: JSON.stringify({name: "Alex"}),
+      headers: {
+          'Content-type': 'application/json'
+      }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json));
 
