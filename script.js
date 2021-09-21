cbbe94d04ff33e2d7e8cec3c1bb3c368d3a98c68
Vladislav <vladislav.koselev@ivkhk.ee>
2021-09-21 11:08:48 +0300

    5

--- script.js
+++ script.js
@@ -1,22 +1,251 @@
-//Fetch API
+"use strict"
+
+//get, set
+
+const person = {
+    name: "John",
+    age: 28,
+
+    get userAge() {
+        return this.age;
+    },
+    set userAge(num){
+        this.age = num;
+    }
+};
+
+
+console.log(person.userAge);
+console.log(person.userAge = 30);
+console.log(person.userAge);
+
+//Инкапцуляция
+
+function User (name, age){
+    this.name = name;
+    this.age = age;
+
+    this.say = function (){
+        console.log(`Имя пользователя : ${this.name}, возраст ${this.age}`);
+    };
+}
+
+const john = new User ('John', 22);
+john.age = 30;
+console.log(john.name);
+console.log(john.age);
+john.say();
+
+//c inkapsylyaciyey
+
+function User2 (name, age) {
+    this.name = name;
+    let userAge = age;
+
+    this.say = function(){
+        console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+    };
+}
+
+const ann = new User2 ('Ann', 22);
+
+console.log(ann.name);
+console.log(ann.userAge);
+ann.say();
+
+////////////////////////////////////////////////
+
+
+function User3 (name, age) {
+    this.name = name;
+    let userAge = age;
+
+    this.say = function () {
+        console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+    };
+
+    this.getAge = function() {
+        return userAge;
+    }
+    this.setAge = function(age){
+        if(typeof age === 'number' && age > 0 && age <100) {
+            userAge = age;
+        }else{
+            console.log('недопустимое значение!')
+        }
+    }
+}
+
+const kirill = new User3('Kirill', 25);
+
+console.log(kirill.name);
+console.log(kirill.userAge);
+console.log(kirill.getAge());
+kirill.setAge(33);
+kirill.setAge(333);
+kirill.setAge('fwefwe');
+kirill.say();
+
+/////////////////////////////
+//
+
+
+class Author {
+    constructor (name,age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Maal'; //private pole
+
+    say = () => {
+        console.log(`Имя пользователя: ${this.name}, ${this.#surname} возраст ${this._age}`);
+    }
+
+    get age(){
+        return this._age;
+    }
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age <100){
+            this._age = age;
+        }else{
+            console.log('недопустимое значение !')
+
+        }
+    }
+}
+
+const ivan = new Author ('Ivan', 25);
+
+console.log(ivan.name);
+ivan.age = 99;
+console.log(ivan.age);
+
+ivan.say();
+
+//////////////
+"use strict";
+
+const num = 1;
+
+(function(){
+    let num = 2;
+    console.log(num);
+    console.log(num + 1 );
+}());
+
+console.log(num);
+
+const user = (function(){
+    const privat = function () {
+        console.log('I am privat');
+    };
+
+    return {
+        sayHello: privat 
+    }
+}());
+
+user.sayHello();
+
+
+//Вариант 3
+console.log("Запрос данных...");
+const req = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log("подготовка данных...");
+
+        const product = {
+            name: 'phone',
+            price: 300
+        };
+
+        resolve(product);
+    },2000);
+});
+
+req.then((product)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=> {
+            product.status = 'order';
+            //resolve(product);
+            reject();
+        }, 2000);
+    });
+}).then(data=>{
+    data.modify = 'true';
+    return data;
+}).then(data=>{
+    console.log(data);
+}).catch(()=>{
+    console.error('ERROR!')
+}
+);
+
+//Вариант 4 финальный
+console.log ("запрос данных...");
+const req = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log("Подготовка данных...");
+
+        const product = {
+            name: 'phone',
+            price: 300
+        };
+
+        resolve(product);
+    },2000);
+});
+
+req.then((product)=>{
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            product.status = 'order';
+            resolve(product);
+            reject();
+        },2000);
+    });
+}).then(data=>{
+    data.modify = 'true';
+    return data;
+}).then (data=> {
+    console.log(data);
+}).catch(()=>{
+    console.error('ERROR!')
+}).finally(()=>{
+    console.log("Все действия выполнены !");
+});
+
+//------------------------------------------------
+const test  = time => {
+    return new Promise ( resolve => {
+        setTimeout (()=> resolve(), time);
+    });
+};
+
+test(1000).then(() => console.log('1000 ms'));
+test(2000).then(() => console.log('2000 ms'));
+
+Promise.all([test(4000),  test (1000)]).then (()=>{
+    console.log('All');
+});
+
+//FETCH API  
 
 "use strict";
 
-fetch('http://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json));
+//https://jsonplaceholder.typicode.com/  API
 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response => response.json ())
+.then(json => console.log (json));
 
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: "POST",
-      body: JSON.stringify({name: "Alex"}),
-      headers: {
-          'Content-type': 'application/json'
-      }
-  })
-  .then(response => response.json())
-  .then(json => console.log(json));
-
-
-  
-
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: "POST" ,
+    body: JSON.stringify({name: "Alex"}),
+    headers: {
+        'Content-type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(json => console.log(json));
